862a8ea01a8f511c8c425f392c7fc4a90fe0bb4c
shjang1025 <shjang1025@gmail.com>
2024-02-28 14:14:57 -0800

    finish addNumbers

--- callbackExercise.js
+++ callbackExercise.js
@@ -1,3 +1,5 @@
+
+
 class Clock {
     constructor() {
       // 1. Create a Date object.
@@ -40,4 +42,28 @@ class Clock {
     }
   }
 
-  const clock = new Clock();
+  // const clock = new Clock();
+
+
+const readline = require('readline');
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function addNumbers (sum, numsLeft, completionCallback) {
+  if (numsLeft > 0 ) {
+    reader.question("Enter the number", (number) => {
+      sum += parseInt(number);
+      console.log(sum);
+      numsLeft -= 1;
+      addNumbers (sum, numsLeft, completionCallback)
+    });
+  }
+  if (numsLeft === 0) {
+    reader.close();
+    completionCallback(sum);
+  }
+}
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
